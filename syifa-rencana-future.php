050b6d28bae3d0a817b1fe7a7ba182e3f31a2169
Rosiyah <100784120+Rosiyah@users.noreply.github.com>
2022-03-02 22:36:15 +0700

    Bikin baru syifa-rencana-future.php

---
+++ syifa-rencana-future.php
@@ -0,0 +1,5 @@
+Rencana Syifa 
+- Bersedia untuk selalu belajar untuk study s1 lebih baik
+-Syifa sebentar lagi akan lulus s1 
+- Syifa akan lulus s1 tiga setengah tahun 
+ammiiinn semoga doanya di ijabah
