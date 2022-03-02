ebe49ca8cba266b1bccb8d8e3ab3aa5b787398ec
Rosiyah <100784120+Rosiyah@users.noreply.github.com>
2022-03-02 22:15:35 +0700

    Create syifa-projek.php

---
+++ syifa-projek.php
@@ -0,0 +1,4 @@
+Syifa projek
+Saya pengen tugas saya selalu selesai dengan cara yang terbaik
+semoga saya selalu konsisten dan semangat selalu
+tidak usah mengeluh dengan gegermu yang pegel nabimu jauh lebih capek masa dewasanya
