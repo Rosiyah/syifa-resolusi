a4c49af482e340845724437162bbf4bb8f41aa37
Rosiyah <100784120+Rosiyah@users.noreply.github.com>
2022-03-02 22:19:06 +0700

    Ini perubahan kedua syifa-projek.php

--- syifa-projek.php
+++ syifa-projek.php
@@ -2,3 +2,5 @@ Syifa projek
 Saya pengen tugas saya selalu selesai dengan cara yang terbaik
 semoga saya selalu konsisten dan semangat selalu
 tidak usah mengeluh dengan gegermu yang pegel nabimu jauh lebih capek masa dewasanya
+jangan cepat menyerah atau bahkan mengeluh 
+patahkan selalu hal yang membuatmu menjadi low value 
