521a1b28d40c2a55bc7989140609b03cc46ff8d1
Rosiyah <100784120+Rosiyah@users.noreply.github.com>
2022-04-06 15:33:21 +0700

    Delete syifa-rencana-future.php

--- syifa-rencana-future.php
+++
@@ -1,5 +0,0 @@
-Rencana Syifa 
-- Bersedia untuk selalu belajar untuk study s1 lebih baik
--Syifa sebentar lagi akan lulus s1 
-- Syifa akan lulus s1 tiga setengah tahun 
-ammiiinn semoga doanya di ijabah
